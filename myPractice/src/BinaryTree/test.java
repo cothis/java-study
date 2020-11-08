578af2717a3d87969c7cf0f9c79c3b3b82aebb8a
cothis <minho1030@naver.com>
2020-11-08 21:28:18 +0900

    add linked list

--- myPractice/src/BinaryTree/test.java
+++ myPractice/src/BinaryTree/test.java
@@ -1,7 +1,8 @@
 package BinaryTree;
 
 public class test {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+	System.out.println("안녕하세요.");
+	System.out.println("테스트입니다.");	
+    }
 }
