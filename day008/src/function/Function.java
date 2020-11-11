bed575247f67df9085aa3a32cdc0ef3a27c3d735
cothis <minho1030@naver.com>
2020-11-11 09:45:40 +0900

    init day008

---
+++ day008/src/function/Function.java
@@ -0,0 +1,20 @@
+package function;
+
+public class Function {
+	public static void main(String[] args) {
+		/*
+		function 복습
+			- 함수 == method
+			- 들어가는 값 - > 인자, 인수, parameter, 매개변수(가인수)
+			- 나오는 값 -> return 값
+		
+		나오는 값의 자료형 함수명 (들어가는 값(0개 이상))
+		{
+			처리 내용
+			
+			return 나오는 값
+		}
+	    
+		*/
+	}
+}
